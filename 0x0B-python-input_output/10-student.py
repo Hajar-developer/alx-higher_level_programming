12df7870cd568078d665a7836f8ce9a196a87f59
Hajar-developer <hajarelfadil2001@gmail.com>
2024-01-09 12:23:33 -0800

    input output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""This module defines a class Student"""
+"""Student to JSON module"""
 
 
 class Student:
-    """Represent a student."""
-
+    """defines a student"""
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
-        """
+        """initializes a student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student
-        If attrs is a list of strings, represents only those attributes
-        included in the list
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict___
+        """dictionary representation of a Student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
