4d091bf776ac0e86f53fb5d27b62c43d8742debc
Hajar-developer <hajarelfadil2001@gmail.com>
2024-04-25 15:41:20 -0700

    python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-"""Algorithms for list of integers."""
+""" Module that finds a peak in a list of unsorted integers """
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers."""
-    if list_of_integers:
-        list_of_integers.sort(reverse=True)
+    """ Function that finds a peak in a list of unsorted integers """
+    if len(list_of_integers) == 0:
+        return None
+    elif len(list_of_integers) == 1:
         return list_of_integers[0]
-    
+    elif len(list_of_integers) == 2:
+        return max(list_of_integers)
+    else:
+        mid = int(len(list_of_integers) / 2)
+        if list_of_integers[mid] > list_of_integers[mid - 1] and\
+           list_of_integers[mid] > list_of_integers[mid + 1]:
+            return list_of_integers[mid]
+        elif list_of_integers[mid] < list_of_integers[mid - 1]:
+            return find_peak(list_of_integers[:mid])
+        else:
+            return find_peak(list_of_integers[mid + 1:])
